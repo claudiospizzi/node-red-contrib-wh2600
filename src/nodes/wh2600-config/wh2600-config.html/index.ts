82227eeb7edf8e115cf3cbdc7d847b7b0b64f53b
Claudio Spizzi <claudio@spizzi.net>
2021-07-13 00:38:07 +0200

    Update editor

--- src/nodes/wh2600-config/wh2600-config.html/index.ts
+++ src/nodes/wh2600-config/wh2600-config.html/index.ts
@@ -6,8 +6,14 @@ declare const RED: EditorRED;
 RED.nodes.registerType<WH2600ConfigEditorNodeProperties>("wh2600-config", {
   category: "config",
   defaults: {
-    name: { value: "", required: true },
-    address: { value: "", required: true },
+    name: {
+      value: "",
+      required: true
+    },
+    address: {
+      value: "",
+      required: true
+    },
   },
   label: function () {
     return this.name || "wh2600 config";
